cb4efa20bf69c683fa1aee74e2af514a291f65a4
OatCoE15 <nithiporndacha@gmail.com>
2021-04-28 05:26:50 +0700

    link Button

--- mini_project/pages/index.js
+++ mini_project/pages/index.js
@@ -2,7 +2,10 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
 import axios from "axios";
 import { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
   const [usd, setUSD] = useState(0);
   const [thb, setTHB] = useState(0);
   const [jpy, setJPY] = useState(0);
@@ -26,7 +29,9 @@ export default function Home() {
       <div className={styles.homepage}>
         <p className={styles.indexTitle}>เสื้อผ้าแฟนชั่น</p>
         <p className={styles.subtitle}>คอลเลคชั่นใหม่</p>
-        <button className={styles.indexButton}>เลือกชมสินค้า</button>
+        <button className={styles.indexButton} onClick={() => {
+            router.push("/shop");
+          }}>เลือกชมสินค้า</button>
       </div>
       <div className="rows" style={{ marginTop: "-10px" }}>
         <p className={styles.curencytitle}>อัตราแลกเปลี่ยนเงินตรา 1 EUR :</p>
